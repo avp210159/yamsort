9c61aa4b43f1fc8e52aa42d526be56093de4c579
avp210159@gmail.com <avp210159@gmail.com@5649c970-96ed-e46e-ae5c-3f0572241d32>
2012-09-02 16:17:59 +0000

    measure.c: Fix Costi from GB to KB, asm64Sort.c reuse tmp[] in RadixSort

--- asm64_Sort.c
+++ asm64_Sort.c
@@ -15,15 +15,39 @@ asm64_intqs (int *a, size_t n)
 #endif
 }
 
+static int *tmp = NULL, ntmp;
+
+void
+asm64_freetmp ()
+{
+  if (tmp)
+    free(tmp);
+}
+
 void
 asm64_intrs (int *a, size_t n)
 {
 #ifdef ASM64
-  int *tmp = malloc(n*sizeof(*a));
-  if (!tmp) 
-    printf ("No memory for tmp array\n");
-  else
-    Array_RadixSortAsc_sint32(a,tmp,n);
+  if (!tmp) {
+    if (!(tmp = malloc(n*sizeof(*a)))) {
+      printf ("No memory for tmp array\n");
+      return;
+    }
+    ntmp = n;
+  }
+
+  if (n > ntmp) {
+    int *old = tmp;
+    if (!(tmp = realloc(tmp,n*sizeof(*a)))) {
+      printf ("No memory for tmp array\n");
+      free(old);
+      return;
+    }
+    ntmp = n;
+  }
+
+  Array_RadixSortAsc_sint32(a,tmp,n);
+
 #else
   printf ("Array_RadixSortAsc_sint32() Not implemented\n");
 #endif
